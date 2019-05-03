cc74505b5b88562eeeccd2809541a915bdb62483
Agnieszka Koszany <agnieszka.koszany@codecool.com>
2019-05-03 16:38:40 +0200

    Move wagegap app to the main page

--- wagegap/urls.py
+++ wagegap/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.contact_view, name='your-wage')
     #url(r'^search-form/$', views.search_form),
 ]
