d3034b4f38608f6c5852485f0ed527153668eee0
Agnieszka Koszany <agnieszka.koszany@codecool.com>
2019-05-14 17:32:15 +0200

    Add __str__ method for all models

--- wagegap/models.py
+++ wagegap/models.py
@@ -3,24 +3,38 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import ssl
 
-
 class Country(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class MinWage(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     wage = models.IntegerField()
     last_update = models.DateField()
 
+    def __str__(self):
+        return 'Minimum wage in ' + str(self.country) + ': ' + str(self.wage)
+
 class Category(models.Model):
     name = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Spending(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 class Cost(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     spending = models.ForeignKey(Spending, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     last_update = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return str(self.spending) + ' costs ' + str(self.price) + ' in ' + str(self.country)
